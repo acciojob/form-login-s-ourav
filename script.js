f644b5ad3f4b4f11d9a4b7d74b99621b008af8a8
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-25 15:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	const submitButton = document.querySelector('input[type="submit"]');
+	fname=document.getElementById('fname').value
+	lname=document.getElementById('lname').value
+	name= fname+" "+lname
+	submitButton.addEventListener(("click"),()=>{alert(name)})
 }
